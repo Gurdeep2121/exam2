a8f280dab636a30d3a0a47167f42e152d382afda
Gurdeep2121 <gurdeep.singh60@qmail.cuny.edu>
2024-11-18 18:41:35 -0500

    Initial commit

---
+++ app/components/Footer.tsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <>
+<footer class="footer footer-center bg-base-300 text-base-content p-4">
+  <aside>
+    <p>Copyright © {new Date().getFullYear()} - All right reserved by The New York Knicks</p>
+  </aside>
+</footer>
+
+</>
+
+
+)
+}
